65d649dc166af44b59839750b119f8833a668318
Limeng <baloo@foxmail.com>
2018-04-02 13:46:20 +0800

    beta

---
+++ src/main/java/com/baloo/nozama/mapper/OrderItemMapper.java
@@ -0,0 +1,21 @@
+package com.baloo.nozama.mapper;
+
+import com.baloo.nozama.pojo.OrderItem;
+import com.baloo.nozama.pojo.OrderItemExample;
+import java.util.List;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    List<OrderItem> selectByExample(OrderItemExample example);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/baloo/nozama/mapper/ProductImageMapper.java
@@ -0,0 +1,21 @@
+package com.baloo.nozama.mapper;
+
+import com.baloo.nozama.pojo.ProductImage;
+import com.baloo.nozama.pojo.ProductImageExample;
+import java.util.List;
+
+public interface ProductImageMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProductImage record);
+
+    int insertSelective(ProductImage record);
+
+    List<ProductImage> selectByExample(ProductImageExample example);
+
+    ProductImage selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProductImage record);
+
+    int updateByPrimaryKey(ProductImage record);
+}
